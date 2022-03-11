367c7c4034ded685fa784bc7384d30ebead0bb30
Ellenilu <ellenieyasu@gmail.comgit config --global user.name Ellenilugit config --global user.email ellenieyasu@gmail.com>
2022-03-11 14:57:04 -0800

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
 
-/**
- * main - Prints all possible combinations of two two-digit numbers,
- *	  ranging from 0-99, separated by a comma followed by a space.
- *
- * Return: Always 0.
- */
+
 int main(void)
 {
 	int digit1, digit2, digit3;
