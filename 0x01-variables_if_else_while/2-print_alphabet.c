0c868414f41944a9dbebc28a7a8389091b8c5752
Ellenilu <ellenieyasu@gmail.comgit config --global user.name Ellenilugit config --global user.email ellenieyasu@gmail.com>
2022-03-11 04:47:50 -0800

    2-print_alphabet.c commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabet in lowercase
+ *
+ * Description: print the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 97;
+
+	while (i < 123)
+	{
+		putchar(1);
+		i++;
+	}
+	putchar(10);
+
+	return (0);
+}
