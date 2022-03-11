e9adba6faa7d0caaaaa01429bce400c4f77227dd
Ellenilu <ellenieyasu@gmail.comgit config --global user.name Ellenilugit config --global user.email ellenieyasu@gmail.com>
2022-03-11 11:57:51 -0800

    5-print_numbers.c commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabet in lowercase
+ *
+ * Description: print the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+
+	printf(i);
+	printf('\n');
+	return (0);
+}
