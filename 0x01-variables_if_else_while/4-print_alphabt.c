face56686f2a0c6286771180652a53b9d59ea021
Ellenilu <ellenieyasu@gmail.comgit config --global user.name Ellenilugit config --global user.email ellenieyasu@gmail.com>
2022-03-11 06:37:55 -0800

    4-print_alphabt.c commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -14,13 +14,14 @@ int main(void)
 {
 	int i;
 
-	for (i = 'a'; i <= 'z'; i++)
+	for (i = 'a'; i <= 'z'; i++){
+	
 	if (i == 'e' || i == 'q')
 	{
 		continue;
 	}
-
-	putchar(i);
+	putchar(i);                                                                                        
 	putchar('\n');
 	return (0);
+}	
 }
