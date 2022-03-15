d0918d3a04187d6822d0a052a01b8a7b6ff32224
Ellenilu <ellenieyasu@gmail.comgit config --global user.name Ellenilugit config --global user.email ellenieyasu@gmail.com>
2022-03-15 13:28:13 -0700

    Revert "commit print putchar"
    This reverts commit 6925059e499b768eba70e8913525329929a3fb8b.

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabet in lowercase
+ *
+ * Description: print the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 'z'; i >= 'a'; i--)
+
+	putchar(i);
+	putchar('\n');
+	return (0);
+}
